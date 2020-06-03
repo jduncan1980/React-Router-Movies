66a523d3fa262e42dfa5d386a8fd2823953eb2d9
Jason Duncan <jasonduncan@Jasons-MacBook-Pro.local>
2020-06-03 04:40:36 -0400

    mvp

--- client/src/Movies/MovieList.js
+++ client/src/Movies/MovieList.js
@@ -1,35 +1,37 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 
-const MovieList = props => {
-  return (
-    <div className="movie-list">
-      {props.movies.map(movie => (
-        <MovieDetails key={movie.id} movie={movie} />
-      ))}
-    </div>
-  );
-}
+const MovieList = (props) => {
+	return (
+		<div className='movie-list'>
+			{props.movies.map((movie) => (
+				<MovieDetails key={movie.id} movie={movie} />
+			))}
+		</div>
+	);
+};
 
 function MovieDetails({ movie }) {
-  const { title, director, metascore, stars } = movie;
-  return (
-    <div className="movie-card">
-      <h2>{title}</h2>
-      <div className="movie-director">
-        Director: <em>{director}</em>
-      </div>
-      <div className="movie-metascore">
-        Metascore: <strong>{metascore}</strong>
-      </div>
-      <h3>Actors</h3>
-
-      {stars.map(star => (
-        <div key={star} className="movie-star">
-          {star}
-        </div>
-      ))}
-    </div>
-  );
+	const { title, director, metascore, stars } = movie;
+	return (
+		<Link to={`/movies/${movie.id}`}>
+			<div className='movie-card'>
+				<h2>{title}</h2>${movie.id}
+				<div className='movie-director'>
+					Director: <em>{director}</em>
+				</div>
+				<div className='movie-metascore'>
+					Metascore: <strong>{metascore}</strong>
+				</div>
+				<h3>Actors</h3>
+				{stars.map((star) => (
+					<div key={star} className='movie-star'>
+						{star}
+					</div>
+				))}
+			</div>
+		</Link>
+	);
 }
 
 export default MovieList;
